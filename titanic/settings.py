b31b1d4bec8777ff373d1fbb778fcb7ab56400ea
Nirmal <Nirmal@admin.com>
2023-09-09 10:00:13 +0400

    new

--- titanic/settings.py
+++ titanic/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z*60m_o0z06@%=ict!n0+a&c_$6i*t=(xps)4xx)8s$!0s$u3n'
+# SECRET_KEY = 'django-insecure-z*60m_o0z06@%=ict!n0+a&c_$6i*t=(xps)4xx)8s$!0s$u3n'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
